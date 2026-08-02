7df7057407bf0ad2a8c67c1606a0625aa66905ee
md ashraful islam <muhammadashrafulislam770@gmail.com>
2026-08-02 22:13:08 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
 from datetime import date
+from ultils import add, subtract
 print("MD. Ashraful Islam")
 print(f"Today's Date: {date.today()}")
+
+print("Sum:", add(10, 5))
+print("Difference:", subtract(10, 5))
